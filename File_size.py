8fdd0df4d51dcc7f6f3011689fb1fc840eaf9a86
JOSPHATT <kimwelemutunga@gmail.com>
2023-06-14 18:45:32 +0300

    Update File_size.py

--- File_size.py
+++ File_size.py
@@ -2,5 +2,5 @@
 # using getsize function os.path module
 import os
  
-file_size = os.path.getsize('d:/file.jpg')
+file_size = os.path.getsize('https://github.com/JOSPHATT/REALTIMEFOREX.py/blob/main/realtimeforex.txt')
 print("File Size is :", file_size, "bytes")
