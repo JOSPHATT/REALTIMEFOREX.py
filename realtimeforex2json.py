dfd8d3f705845f93bd735a167654736132264386
JOSPHATT <kimwelemutunga@gmail.com>
2023-06-27 22:22:51 +0300

    Update realtimeforex2json.py

--- realtimeforex2json.py
+++ realtimeforex2json.py
@@ -1,4 +1,28 @@
-import realtimeforexex
+from realtimeforex import *
+import json
 
-print(finaldata)
-print(time_stamp)
+
+#finaldata=final_data()
+time_stamp=timerer()
+
+Final_data={}                                              Final_data[time_stamp]=finaldata
+#print(Final_data)
+
+#Final_data_dictionary[time_stamp]
+
+filename = 'realtimeforex.json'
+# Old JSON File:
+# [{"alice": 24, "bob": 27}]
+entry = final_data()
+
+import os
+
+if os.stat('forex.json').st_size == 0:
+    with open(filename, "w") as file:
+        json.dump(Final_data, file)
+else:
+    with open(filename, "r+") as file:
+        data = json.load(file)
+        data[time_stamp]=entry
+        file.seek(0)
+        json.dump(data, file)
