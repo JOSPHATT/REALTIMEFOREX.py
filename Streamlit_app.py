f3abea79c9a07fba21992d333eeb21e9a146ef1a
JOSPHATT <kimwelemutunga@gmail.com>
2023-07-10 18:12:22 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -11,5 +11,10 @@ def datagen():
   return pd.DataFrame(github_jsonforex_dictionary.values())
 
 st.title('MY LIVE FOREX')
-pairs=[AUDJPY,AUDNZD,AUDUSD,CADJPY,EURJPY,EURUSD,GBPJPY,USDJPY]
-st.line_chart(datagen()['AUDJPY'])
+pairs=["AUDJPY","AUDNZD","AUDUSD","CADJPY","EURJPY","EURUSD","GBPJPY","USDJPY"]
+tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8 = st.tabs([i for i in pairs])
+tab1.subheader("AUDJPY")
+tab1.line_chart(datagen()['AUDJPY'])
+
+tab2.subheader("AUDUSD")
+tab2.write(datagen()['AUDUSD'])
