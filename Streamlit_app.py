631276c0eab2ae0429353b3d9799544f0822631e
JOSPHATT <kimwelemutunga@gmail.com>
2023-07-10 17:55:14 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,17 +4,12 @@ import requests
 github_url="https://raw.githubusercontent.com/JOSPHATT/REALTIMEFOREX.py/main/realtimeforex.json"
 import json
 
-with open('realtimeforex.json') as f:
-  github_jsonforex_dictionary = json.load(f)
-
 import pandas as pd
-from pandas import json_normalize
-column_names=[i for i in github_jsonforex_dictionary.keys()]
 def datagen():
+  with open('realtimeforex.json') as f:
+    github_jsonforex_dictionary = json.load(f)
   return pd.DataFrame(github_jsonforex_dictionary.values())
+
 st.title('MY LIVE FOREX')
 st.header('AUDJPY')
-df=datagen()
-st.dataframe(df)
-AUDJPY_data=df['AUDJPY']
-st.line_chart(AUDJPY_data)
+st.line_chart(datagen()['AUDJPY'])
