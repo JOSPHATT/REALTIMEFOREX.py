775e08c6fb72757944306a80e5796ea7f56fb6d9
JOSPHATT <kimwelemutunga@gmail.com>
2023-07-10 17:08:42 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,10 +4,8 @@ import requests
 github_url="https://raw.githubusercontent.com/JOSPHATT/REALTIMEFOREX.py/main/realtimeforex.json"
 import json
 p = requests.get(github_url)
-p.text
 github_jsonforex_dictionary=json.loads(p.text)
 p = requests.get(github_url)
-p.text
 github_jsonforex_dictionary=json.loads(p.text)
 #Total_entries=print(len(github_jsonforex_dictionary.values()))
 
